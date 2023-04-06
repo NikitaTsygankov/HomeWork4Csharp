a558c2b602ff5425169cae20a633889c06c769ae
Nikita Tsygankov <like_fender@mail.ru>
2023-04-06 19:17:01 +0300

    Task 1 changed and task 2 added

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,4 +1,8 @@
-﻿Console.Write("Введите число A: ");
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в 
+// натуральную степень B
+
+
+Console.Write("Введите число A: ");
 int a = int.Parse(Console.ReadLine());
 Console.Write("Введите число B: ");
 int b = int.Parse(Console.ReadLine());
