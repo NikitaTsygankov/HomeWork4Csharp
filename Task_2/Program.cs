84813e80ffa9f12e57b6d18f4a8d2b092172353d
Nikita Tsygankov <like_fender@mail.ru>
2023-04-06 19:23:37 +0300

    task 2 changed

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -1,1 +1,29 @@
-﻿
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе
+
+Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+int len = Length(a);
+Console.Write($"Сумма цифр в числе {a} равно: {Sum(a,len)}");
+
+// Подсчет количества символов в числе
+int Length(int a)
+{
+int index = 0;
+while (a > 0)
+{
+a /= 10;
+index++;
+}
+return index;
+}
+
+int Sum(int a, int len)
+{
+int sum = 0;
+for (int i = 1; i <= len; i++)
+{
+sum += a % 10;
+a /= 10;
+}
+return sum;
+}
